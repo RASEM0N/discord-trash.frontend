a77f0ddd57419c58fe870c2a13175205d08cf0f5
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-03-27 19:18:17 +0500

    Create routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+import Login from './components/Auth/Login'
+import Register from './components/Auth/Register'
+import App from './App.jsx'
+
+const Root = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path={'/'} component={App} />
+            <Route path={'/login'} component={Login} />
+            <Route path={'/register'} component={Register} />
+        </Switch>
+    </BrowserRouter>
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+    <React.StrictMode>
+        <Root />
+    </React.StrictMode>,
+    document.getElementById('root')
+)
