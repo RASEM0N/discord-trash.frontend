859656028fedae09ece94cfa089fe5604ec31bb2
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-03-28 20:27:11 +0500

    User Panel

--- src/components/SIdePanel/SidePanel.jsx
+++ src/components/SIdePanel/SidePanel.jsx
@@ -1,6 +1,21 @@
 import React from 'react'
+import { Menu } from 'semantic-ui-react'
+import UserPanel from './UserPanel'
 
 const SidePanel = () => {
-    return <div>SidePanel</div>
+    return (
+        <Menu
+            size="large"
+            inverted
+            fixed="left"
+            vertical
+            style={{
+                fontsize: '1.2rem',
+                background: 'grey',
+            }}
+        >
+            <UserPanel />
+        </Menu>
+    )
 }
 export default SidePanel
---
+++ src/components/SIdePanel/UserPanel.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Dropdown, Grid, Header, Icon, Menu } from 'semantic-ui-react'
+
+const UserPanel = () => {
+    const dropdownOptions = [
+        {
+            key: 'user',
+            text: (
+                <span>
+                    Signed is as <strong>User</strong>{' '}
+                </span>
+            ),
+            disabled: true,
+        },
+
+        {
+            key: 'avatar',
+            text: <span>Change a Avatar</span>,
+        },
+
+        {
+            key: 'signout',
+            text: <span>Sing Out</span>,
+        },
+    ]
+
+    return (
+        <Grid>
+            <Grid.Column>
+                <Grid.Row
+                    style={{
+                        padding: '1.2rem',
+                        margin: 0,
+                    }}
+                >
+                    {/*App Header*/}
+                    <Header inverted floated="left" as="h2">
+                        <Icon className="chat" />
+                        <Header.Content>Chat</Header.Content>
+                    </Header>
+                </Grid.Row>
+                {/*User Dropdown*/}
+                <Header
+                    style={{
+                        padding: '0.25rem',
+                    }}
+                    inverted
+                    as="h4"
+                >
+                    <Dropdown
+                        trigger={<span>User</span>}
+                        options={dropdownOptions}
+                    />
+                </Header>
+            </Grid.Column>
+        </Grid>
+    )
+}
+export default UserPanel
