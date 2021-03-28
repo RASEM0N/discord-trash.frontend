51073f0646b3b9efa4af92fc27f1e4b725cb0bf8
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-03-28 14:36:06 +0500

    Lister for Login & Register

--- src/index.js
+++ src/index.js
@@ -1,25 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, withRouter } from 'react-router-dom'
 import Login from './components/Auth/Login'
 import Register from './components/Auth/Register'
 import 'semantic-ui-css/semantic.min.css'
 import './App.css'
 import App from './App.jsx'
+import firebase from './firebase/firebase'
 
-const Root = () => (
-    <BrowserRouter>
+const Root = ({ history }) => {
+    useEffect(() => {
+        // вроде куки чекает
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                history.push('/')
+            }
+        })
+    }, [])
+
+    return (
         <Switch>
             <Route exact path={'/'} component={App} />
             <Route path={'/login'} component={Login} />
             <Route path={'/register'} component={Register} />
         </Switch>
-    </BrowserRouter>
-)
+    )
+}
+
+const RootWithAuth = withRouter(Root)
 
 ReactDOM.render(
-    // <React.StrictMode>
-    <Root />,
-    // </React.StrictMode>
+    <BrowserRouter>
+        <RootWithAuth />
+    </BrowserRouter>,
     document.getElementById('root')
 )
