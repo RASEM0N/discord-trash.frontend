0f1af16061e2614cacd388500d36f2a5242f42a0
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-03-29 11:53:22 +0500

    Color Pa nel + Messages

---
+++ src/components/Messages/MessageForm.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react'
+import { Segment, Input, Button } from 'semantic-ui-react'
+import firebase from '../../firebase/firebase'
+
+const MessagesForm = ({
+    messageRef,
+    currentChannel,
+    currentUser: { uid, photoURL, displayName },
+}) => {
+    const [message, setMessage] = useState('')
+    const [loading, setLoading] = useState(false)
+
+    const onChange = (e) => {
+        setMessage(e.target.value)
+    }
+
+    const sendMessage = () => {
+        if (message) {
+            setLoading(true)
+            messageRef
+                .child(currentChannel.id)
+                .push()
+                .set({
+                    content: message,
+                    date: firebase.database.ServerValue.TIMESTAMP,
+                    user: {
+                        id: uid,
+                        name: displayName,
+                        avatar: photoURL,
+                    },
+                })
+                .then(() => {
+                    setMessage('')
+                    setLoading(false)
+                })
+                .catch((err) => {
+                    setLoading(false)
+                    console.error(err)
+                })
+        }
+    }
+
+    return (
+        <Segment className="message__form">
+            <Input
+                fluid
+                name="message"
+                style={{ marginBottom: '0.7em' }}
+                label={<Button icon={'add'} />}
+                labelPosition="left"
+                placeholder="Write your message"
+                value={message}
+                onChange={onChange}
+            />
+            <Button.Group icon widths="2">
+                <Button
+                    color="grey"
+                    content="Add Reply"
+                    labelPosition="left"
+                    onClick={sendMessage}
+                    icon="edit"
+                    disabled={loading}
+                />
+                <Button
+                    color="grey"
+                    content="Upload Media"
+                    labelPosition="right"
+                    icon="cloud upload"
+                    disabled={loading}
+                />
+            </Button.Group>
+        </Segment>
+    )
+}
+export default MessagesForm
--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,6 +1,37 @@
 import React from 'react'
+import { Segment, Comment } from 'semantic-ui-react'
+import MessageForm from './MessageForm'
+import MessageHeader from './MessageHeader'
+import firebase from '../../firebase/firebase'
+import { connect } from 'react-redux'
 
-const Messages = () => {
-    return <div>Messages</div>
+const messageRef = firebase.database().ref('messages')
+
+const Messages = ({ currentChannel, currentUser }) => {
+    if (!currentChannel) {
+        return null
+    }
+
+    return (
+        <>
+            <MessageHeader />
+            <Segment>
+                <Comment.Group className="messages">
+                    {/*messages*/}
+                </Comment.Group>
+            </Segment>
+            <MessageForm
+                messageRef={messageRef}
+                currentChannel={currentChannel}
+                currentUser={currentUser}
+            />
+        </>
+    )
 }
-export default Messages
+
+const mapStateToProps = (state) => ({
+    currentChannel: state.channel.currentChannel,
+    currentUser: state.user.currentUser,
+})
+
+export default connect(mapStateToProps, null)(Messages)
